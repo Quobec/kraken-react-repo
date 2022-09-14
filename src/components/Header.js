6de8343140dc423aa70289773f1612fc8adebb29
Jakub Kępinski <jakubkepinski87@gmail.com>
2022-09-14 18:34:20 +0200

    ??

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-import './Header.css';
 
 function Header(){
  	return (
         <>
             <header id="header_id">
-                <a href="index.js"><img src="public/logo512.png" alt="Shop logo image"/></a>
+                <a href="index.js"><img src="" alt="Shop logo image"/></a>
             </header>
         </>
     );
